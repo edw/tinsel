10ed03f5770d28495fb92d379f76230bb3c2d888
Edwin Watkeys <edw@poseur.com>
2013-12-24 19:19:32 -0500

    rev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject edw/tinsel "0.1.0"
+(defproject edw/tinsel "0.2.0"
   :description "Tinsel: shiny threading macros with tracing"
   :url "http://github.com/edw/tinsel"
   :license {:name "Eclipse Public License"
